ba45c39eed41eb832fff3adb3c45c782481bf478
Konrad Sokolowski <konrad-s@outlook.com>
2024-07-09 17:54:50 +0200

    refactor(ruff): adhere to ruff

--- mltc/validator.py
+++ mltc/validator.py
@@ -1,33 +1,56 @@
 import uuid
+from pathlib import Path
+
 import nbformat
 
 
 class NotebookValidator:
-    @staticmethod
-    def add_ids_to_cells(nb):
-        """
-        Adds a unique 'id' field to each cell in the notebook.
-        """
-        for cell in nb.cells:
-            if "id" not in cell.metadata:
-                cell.metadata["id"] = str(uuid.uuid4())  # sssign a new UUID as the ID
-        return nb
+    """A class for validating and modifying Jupyter notebooks.
+
+    This class provides methods to add unique IDs to cells in a notebook and validate the notebook's format.
+
+    Raises:
+        RuntimeError: If the notebook fails validation checks.
+    """
 
     @staticmethod
-    def validate_and_add_ids(nb_path):
+    def add_ids_to_cells(notebook: nbformat.NotebookNode) -> nbformat.NotebookNode:
+        """Adds a unique 'id' field to each cell in the notebook if it doesn't already have one.
+
+        Args:
+            notebook (NotebookNode): A notebook object loaded via nbformat.
+
+        Returns:
+            NotebookNode: The modified notebook object with 'id' fields added to each cell.
         """
-        Validates the notebook and ensures that each cell has a unique 'id' field.
-        Throws an error if the notebook cannot be merged.
+        for cell in notebook.cells:
+            if "id" not in cell.metadata:
+                cell.metadata["id"] = str(uuid.uuid4())  # assign a new UUID as the ID
+        return notebook
+
+    @staticmethod
+    def validate_and_add_ids(path: str) -> nbformat.NotebookNode:
+        """Validates the notebook's format and ensures that each cell has a unique 'id' field.
+
+        Raises an error if the notebook cannot be validated.
+
+        Args:
+            path (str): The file path to the notebook.
+
+        Returns:
+            NotebookNode: The validated and modified notebook object with 'id' fields added to each cell.
+
+        Raises:
+            RuntimeError: If the notebook fails validation checks.
         """
-        with open(nb_path) as f:
+        with Path.open(path) as f:
             nb = nbformat.read(f, as_version=4)
             nb = NotebookValidator.add_ids_to_cells(
-                nb
+                nb,
             )  # ensure all cells have an 'id' field
             try:
                 nbformat.validate(nb)
             except nbformat.ValidationError:
-                raise RuntimeError(
-                    f"The notebook {nb_path} is incapable of merging due to validation errors."
-                )
+                err_msg = f"The notebook {path} is incapable of merging due to validation errors."
+                raise RuntimeError(err_msg) from None
         return nb
